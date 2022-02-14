63636345256b5fdc01e4c1212081f4b188bf5cd5
Aaron Weidele <>
2022-02-14 10:59:23 -0500

    Prettier

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,32 @@
-const linkList = document.querySelector('.link-list');
-const calendar = document.querySelector('.header-calendar');
+"use strict";
 
-const renderCalendar = function() {
-    const months = [
-        'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
-    ];
-    const today = new Date();
-    const month = today.getMonth();
-    const day = today.getDate();
-    const year = today.getFullYear();
-    
-    const firstOfMonth = new Date(year, month, 1, 0, 0 ,0).getDay();
-    const daysInMonth = new Date(year, month + 1, 0, 0, 0 ,0).getDate();
+const linkList = document.querySelector(".link-list");
+const calendar = document.querySelector(".header-calendar");
 
-    return `
+const renderCalendar = function () {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const today = new Date();
+  const month = today.getMonth();
+  const day = today.getDate();
+  const year = today.getFullYear();
+
+  const firstOfMonth = new Date(year, month, 1, 0, 0, 0).getDay() + 1;
+  const daysInMonth = new Date(year, month + 1, 0, 0, 0, 0).getDate();
+
+  return `
         <div class="calendar">
             <div class="calendar__today">
                 <span>${months[month]}</span>
@@ -27,66 +40,99 @@ const renderCalendar = function() {
                 <span class="dow">T</span>
                 <span class="dow">F</span>
                 <span class="dow">S</span>
-                ${Array.from(Array(daysInMonth).keys()).map(d => { 
-                    d++;
-                    return `<span${d === day ? ' class="today"' : ''}${d === 1 ? ` style="grid-column-start: ${firstOfMonth + 1}"` : ''}>${d}</span>`;
-                }).join('')}
+                ${Array.from(Array(daysInMonth))
+                  .map(
+                    (_, i) =>
+                      `<span${i + 1 === day ? ' class="today"' : ""}${
+                        i === 0
+                          ? ` style="grid-column-start: ${firstOfMonth}"`
+                          : ""
+                      }>${i + 1}</span>`
+                  )
+                  .join("")}
             </div>
         </div>
     `;
-}
+};
 
 calendar.innerHTML = renderCalendar();
 
-const renderBookmarks = function(bookmarks) {
-    const links = `
+// 1c58086a0dd586e3fd26953775313809
+
+// async function getCurrentPosition() {
+//     return new Promise( (resolve, reject) => {
+//         navigator.geolocation.getCurrentPosition(
+//             position => resolve(position),
+//             error => reject(error)
+//         )
+//     })
+// }
+
+// try {
+//     const position = await getCurrentPosition()
+//     console.log(position)
+//     const lat = position.coords.latitude
+//     const lon = position.coords.longitude
+//     console.log(lat,lon);
+// } catch(err) {
+//     console.log(err)
+//     document.querySelector('p:nth-of-type(1)').innerText = err.message
+// }
+
+const renderBookmarks = function (bookmarks) {
+  const links = `
         <ul>
-            ${bookmarks.map(link => `<li><a href="${link.url}">${link.linkName}</a> (${link.catName})</li>`).join('')}
+            ${bookmarks
+              .map(
+                (link) =>
+                  `<li><a href="${link.url}">${link.linkName}</a> (${link.catName})</li>`
+              )
+              .join("")}
         </ul>
     `;
 
-    linkList.innerHTML = links;
-}
+  linkList.innerHTML = links;
+};
 
 const getBookmarks = async function () {
-    try {
-        const res = await fetch('https://angrychickens.com/portal/test/');
-        const bookmarks = await res.json();
-        renderBookmarks(bookmarks);
-    } catch(err) {
-        console.log(err);
-    }
-}
+  try {
+    const res = await fetch("https://angrychickens.com/portal/test/");
+    const bookmarks = await res.json();
+    renderBookmarks(bookmarks);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const postBookmark = async function () {
-    // const opts = {
-    //     cheese: 'Cheddar'
-    // }
+  // const opts = {
+  //     cheese: 'Cheddar'
+  // }
 
-    let opts = new FormData();
-    opts.append('cheese', 'Cheddar');
+  let opts = new FormData();
+  opts.append("cheese", "Cheddar");
 
-    try {
-        const res = await fetch('https://angrychickens.com/portal/test/test.php', {
-            method: 'POST',
-            body: opts
-        });
-        const bookmarks = await res.json();
-        console.log('postBookmark',bookmarks);
-    } catch(err) {
-        console.error(err);
-    }
-}
+  try {
+    const res = await fetch("https://angrychickens.com/portal/test/test.php", {
+      method: "POST",
+      body: opts,
+    });
+    const bookmarks = await res.json();
+    console.log("postBookmark", bookmarks);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const postBookmark2 = async function () {
-    try {
-        const res = await fetch('https://angrychickens.com/portal/test/test.php');
-        const bookmarks = await res.json();
-        console.log('postBookmark2',bookmarks);
-    } catch(err) {
-        console.error(err);
-    }
-}
+  try {
+    const res = await fetch("https://angrychickens.com/portal/test/test.php");
+    const bookmarks = await res.json();
+    console.log("postBookmark2", bookmarks);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 getBookmarks();
 postBookmark();
